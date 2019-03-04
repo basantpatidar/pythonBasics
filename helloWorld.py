4f379875d4f1583c0b57b2825522842258b3c79e
basantpatidar <basantpatidar33@gmail.com>
2019-03-04 02:41:43 -0500

    variables

---
+++ helloWorld.py
@@ -0,0 +1,19 @@
+print('Hello World!')
+print(5 + 6)
+print(7*2)
+print()
+print('End')
+print("It's easy")
+print("Hello 'Basant'")
+print("Hello" + "Patidar")
+Greetings = "Hello"
+Name = "Basant"
+#line prints Name and Greeting
+print(Greetings + Name)
+print(Greetings +" "+ Name)
+school = input('Enter you school name ')
+print(Name +"'s school's name is "+school)
+spliteString = "String is splited\n in namy \n parts"
+print(spliteString)
+tabFunction = "1\t2\t3\t4\tBasant"
+print (tabFunction)
