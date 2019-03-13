33764637313de48aff7313d5021b9a79928cdfd9
basantpatidar <basantpatidar33@gmail.com>
2019-03-12 21:58:06 -0400

    augmented assignments

---
+++ augmentedAssignments.py
@@ -0,0 +1,29 @@
+x = 23
+x += 1
+print(x)
+
+x -= 4
+print(x)
+
+x *= 5
+print(x)
+
+x /= 4
+print(x)
+
+x **= 2
+print(x)
+
+x %= 60
+print(x)
+
+x = int(x)
+x //= 5
+print(x)
+
+greeting = "Good"
+greeting += " Night "
+print(greeting)
+
+greeting *= 5
+print(greeting)
