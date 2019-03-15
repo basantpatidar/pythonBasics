f065b3f24eee9c5fc946dcb17198acbcb2d2f07a
basantpatidar <basantpatidar33@gmail.com>
2019-03-14 22:55:58 -0400

    list examples all

--- listExamples.py
+++ listExamples.py
@@ -1,29 +1,29 @@
-# city = ["Big", "Better", "Best", "Bright"]
-# city.append("Metro city")
-#
-# for state in city:
-#     print("New York city is "+ state)
+city = ["Big", "Better", "Best", "Bright"]
+city.append("Metro city")
+
+for state in city:
+    print("New York city is "+ state)
 
 
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
-# number = even + odd
-# # number.sort()
-# print(sorted(number))
-# sorted_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# if sorted_numbers == sorted(number):
-#     print("Both lists are same")
-# else:
-#     print("No they are different lists")
+number = even + odd
+number.sort()
+print(sorted(number))
+sorted_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+if sorted_numbers == sorted(number):
+    print("Both lists are same")
+else:
+    print("No they are different lists")
 
 
-# numbers = [even, odd]
-#
-# for number_set in numbers:
-#     print(number_set)
-#
-#     for value in number_set:
-#         print(value)
+numbers = [even, odd]
+
+for number_set in numbers:
+    print(number_set)
+
+    for value in number_set:
+        print(value)
 
 list_1 = []
 list_2 = list()
